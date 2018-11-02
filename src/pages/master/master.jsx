10e0f3d13b52380ee6583398ac143bcd666d0e8b
Gabriel Tubias <gabrieltubiass@gmail.com>
2018-11-02 15:59:05 -0300

    added google analitycs

--- src/pages/master/master.jsx
+++ src/pages/master/master.jsx
@@ -3,11 +3,19 @@ import './master.scss'
 import React, { Component } from 'react'
 import { ToastContainer } from 'react-toastify'
 import Helmet from 'react-helmet'
+import ReactGA from 'react-ga'
 
 import Nav from './nav/nav'
 import Footer from './footer/footer'
 
 class Master extends Component {
+    constructor(props) {
+        super(props)
+
+        ReactGA.initialize('UA-128597591-1')
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }
+
     render() {
         const {
             children
